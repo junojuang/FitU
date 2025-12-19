b0e9fb24edac65d1b47cc7162adb5e73c1b5ee9d
defton290 <70007384+defton290@users.noreply.github.com>
2025-12-19 17:58:14 +0800

    Edit to backend

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("✅ FitU Backend is running successfully!");
+});
+
 app.use("/api/gemini", geminiRoute);
 app.use("/api/recipes", recipeRoutes);
 
